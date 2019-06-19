e566feeaa864cbea340ec0a4995c48efe5746b02
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-19 19:04:35 +0900

    Simlified.

--- 02x-recursions/029-self-recursion-in-c.c
+++ 02x-recursions/029-self-recursion-in-c.c
@@ -3,10 +3,7 @@
 #include <stdio.h>
 
 int main(int argc, const char *const *argv) {
-	if (argc > 101) {
-		return 0;
-	}
-	else {
+	if (argc <= 101) {
 		printf("%3d\n", argc - 1);
 		main(argc + 1, NULL);
 		/* You may not recursively call main function in C++. */
