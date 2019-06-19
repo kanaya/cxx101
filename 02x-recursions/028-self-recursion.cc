313d8c83f8606e0f7ca2c0e109670ce059c0d343
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-19 19:23:30 +0900

    Self recursion by C++.

---
+++ 02x-recursions/028-self-recursion.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, const char *const *argv) {
+	int n;
+	if (argc > 1) {
+		n = std::atoi(argv[1]);		
+	}
+	else {
+		n = 0;
+	}
+	if (n <= 100) {
+		std::fprintf(stdout, "%3d\n", n);
+		++n;
+		char *cmd = new char[256];
+		std::sprintf(cmd, "%s %d", argv[0], n);
+		std::system(cmd);
+		delete[] cmd;
+	}
+	return 0;
+}
+
+// The idea of this work was given by Krzysztof Kamil Jacewicz.
+// https://qr.ae/TWha4c
