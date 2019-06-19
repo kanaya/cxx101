cd28b13c96be49f2bfe16897665f67f583f63e68
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-19 19:00:59 +0900

    Fixed bug of self-recusion-in-c.

--- 02x-recursions/029-self-recursion-in-c.c
+++ 02x-recursions/029-self-recursion-in-c.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 int main(int argc, const char *const *argv) {
-	if (argc > 100) {
+	if (argc > 101) {
 		return 0;
 	}
 	else {
-		printf("%3d\n", argc);
+		printf("%3d\n", argc - 1);
 		main(argc + 1, NULL);
+		/* You may not recursively call main function in C++. */
 	}
 	return 0;
 }
