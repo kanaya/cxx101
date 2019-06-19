43e68fbb205adee67a0b2f931a8c3151e84b4600
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-19 23:34:33 +0900

    Replaced y-combinator version with very simple one.

--- 02x-recursions/027-y-combinator.cc
+++ 02x-recursions/027-y-combinator.cc
@@ -1,59 +1,20 @@
 // C++14
 
 #include <iostream>
-#include <iomanip>
-#include <functional>
-#include <memory>
-
-template <typename T> struct linklist {
-	T value;
-	std::shared_ptr<linklist> next;
-};
-
-template <typename T> auto back(std::shared_ptr<linklist<T>> s) {
-	if (s == nullptr || s->next == nullptr) {
-		return s;
-	}
-	else {
-		return back(s->next);
-	}
-}
-
-template <typename T> auto append(std::shared_ptr<linklist<T>> s, T v) {
-	if (s == nullptr) {
-		s = std::make_shared<linklist<T>>();
-		s->value = v;
-		s->next = nullptr;
-	}
-	else {
-		auto a = std::make_shared<linklist<T>>();
-		auto b = back(s);
-		if (b != nullptr) {
-			a->next = nullptr;
-			a->value = v;
-			b->next = a;
-		}
-	}
-	return s;
-}
 
 int main() {
-	std::shared_ptr<linklist<int>> list = nullptr;
-	for (int i = 0; i <= 100; ++i) {
-		list = append(list, i);
-	}
 	auto Y = [](auto f) {
 		return [=](auto ...args) {
 			return f(f, args...);
 		};
 	};
-	auto print_all = Y([](auto printer, std::shared_ptr<linklist<int>> s) -> void {
-		if (s != nullptr) {
-			std::cout << std::setw(3) << std::setfill('0') << s->value << '\n';
-			printer(printer, s->next);
+	auto print = Y([](auto f, int i, int n) -> void {
+		if (i <= n) {
+			std::cout << i << '\n';
+			f(f, i + 1, n);
 		}			
 	});
-	print_all(list);
+	print(1, 100);
 	std::cout << std::flush;
 	return 0;
 }
