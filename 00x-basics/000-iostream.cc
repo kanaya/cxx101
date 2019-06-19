686f885b785585b898fc8e47fbd9343f6ecb3595
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-19 16:36:10 +0900

    00x-basics.

---
+++ 00x-basics/000-iostream.cc
@@ -0,0 +1,117 @@
+#include <iostream>
+
+int main() {
+	std::cout << "000\n";
+	std::cout << "001\n";
+	std::cout << "002\n";
+	std::cout << "003\n";
+	std::cout << "004\n";
+	std::cout << "005\n";
+	std::cout << "006\n";
+	std::cout << "007\n";
+	std::cout << "008\n";
+	std::cout << "009\n";
+
+	std::cout << "010\n";
+	std::cout << "011\n";
+	std::cout << "012\n";
+	std::cout << "013\n";
+	std::cout << "014\n";
+	std::cout << "015\n";
+	std::cout << "016\n";
+	std::cout << "017\n";
+	std::cout << "018\n";
+	std::cout << "019\n";
+
+	std::cout << "020\n";
+	std::cout << "021\n";
+	std::cout << "022\n";
+	std::cout << "023\n";
+	std::cout << "024\n";
+	std::cout << "025\n";
+	std::cout << "026\n";
+	std::cout << "027\n";
+	std::cout << "028\n";
+	std::cout << "029\n";
+
+	std::cout << "030\n";
+	std::cout << "031\n";
+	std::cout << "032\n";
+	std::cout << "033\n";
+	std::cout << "034\n";
+	std::cout << "035\n";
+	std::cout << "036\n";
+	std::cout << "037\n";
+	std::cout << "038\n";
+	std::cout << "039\n";
+
+	std::cout << "040\n";
+	std::cout << "041\n";
+	std::cout << "042\n";
+	std::cout << "043\n";
+	std::cout << "044\n";
+	std::cout << "045\n";
+	std::cout << "046\n";
+	std::cout << "047\n";
+	std::cout << "048\n";
+	std::cout << "049\n";
+
+	std::cout << "050\n";
+	std::cout << "051\n";
+	std::cout << "052\n";
+	std::cout << "053\n";
+	std::cout << "054\n";
+	std::cout << "055\n";
+	std::cout << "056\n";
+	std::cout << "057\n";
+	std::cout << "058\n";
+	std::cout << "059\n";
+
+	std::cout << "060\n";
+	std::cout << "061\n";
+	std::cout << "062\n";
+	std::cout << "063\n";
+	std::cout << "064\n";
+	std::cout << "065\n";
+	std::cout << "066\n";
+	std::cout << "067\n";
+	std::cout << "068\n";
+	std::cout << "069\n";
+
+	std::cout << "070\n";
+	std::cout << "071\n";
+	std::cout << "072\n";
+	std::cout << "073\n";
+	std::cout << "074\n";
+	std::cout << "075\n";
+	std::cout << "076\n";
+	std::cout << "077\n";
+	std::cout << "078\n";
+	std::cout << "079\n";
+
+	std::cout << "080\n";
+	std::cout << "081\n";
+	std::cout << "082\n";
+	std::cout << "083\n";
+	std::cout << "084\n";
+	std::cout << "085\n";
+	std::cout << "086\n";
+	std::cout << "087\n";
+	std::cout << "088\n";
+	std::cout << "089\n";
+
+	std::cout << "090\n";
+	std::cout << "091\n";
+	std::cout << "092\n";
+	std::cout << "093\n";
+	std::cout << "094\n";
+	std::cout << "095\n";
+	std::cout << "096\n";
+	std::cout << "097\n";
+	std::cout << "098\n";
+	std::cout << "099\n";
+
+	std::cout << "100";
+	std::cout << std::endl;
+	return 0;
+}
