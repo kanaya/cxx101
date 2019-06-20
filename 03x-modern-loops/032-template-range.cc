a73b4bf3998194e461075aa8fb61a1a59f781579
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-20 16:43:11 +0900

    std::index_sequence version.

---
+++ 03x-modern-loops/032-template-range.cc
@@ -0,0 +1,22 @@
+// C++17
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <initializer_list>
+
+template <typename T, T ...v> std::vector<T> make_vector() {
+	return std::vector<T> { v... };
+}
+
+template <typename T, T ...v> std::vector<T> make_vector(std::index_sequence<v...>) {
+	return make_vector<T, v...>();
+}
+
+int main() {
+	for (auto i: make_vector(std::make_index_sequence<101>{})) {
+		std::cout << std::setw(3) << std::setfill('0') << i << '\n';
+	}
+	std::cout << std::flush;
+	return 0;
+}
