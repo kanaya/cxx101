fd7bca305ed23b6394c5245c155f5bbc67171a47
Ichi Kanaya <kanaya@pineapple.cc>
2019-06-20 16:13:46 +0900

    Range function with standard lib.

---
+++ 03x-modern-loops/031-standard-range.cc
@@ -0,0 +1,23 @@
+// C++14
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <utility>
+#include <initializer_list>
+
+template <typename T> auto range(T begin, T end) {
+	auto a = std::vector<size_t>(end - begin);
+	for (int i = begin; i < end; ++i) {
+		a[i - begin] = i;
+	}
+	return a;
+}
+
+int main() {
+	for (auto i: range(0, 101)) {
+		std::cout << std::setw(3) << std::setfill('0') << i << '\n';
+	}
+	std::cout << std::flush;
+	return 0;
+}
